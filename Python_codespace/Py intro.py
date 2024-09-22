a817254822a16c427e29d6e8937d37c11d536d18
Alicia Bonavita <aliciamariabonavita@gmail.com>
2024-09-22 17:49:48 -0400

    done

--- Python_codespace/Py intro.py
+++ Python_codespace/Py intro.py
@@ -20,7 +20,7 @@ f = open("demofile.txt", "rt")
 # A. open()
 
 # Q By default the file is opened in text mode, but you can also open the file in binary mode. which one of the following syntaxes opens the file in binary mode?
-x = open('demofile.txt', "b")
+#x = open('demofile.txt', "b")
 
 # Q.The defualt opening mode when opening a file with the open() function is "r" for 'reading'
 # A. TRUE
@@ -46,3 +46,43 @@ print(f.readline())
 f = open("demofile.txt", "r")
 print(f.readline())
 f.close()
+
+#Q After opening a file with the open() function, which method can be used to read the context ?
+#A read()
+
+# To read only one line, we can use another method by using the following 
+readline()
+
+# Q. if you call the readline() method two times, it will return the two first lines 
+# A. TRUE
+
+## writing to an exsiting file 
+
+
+f = open("demofile.txt" , "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending 
+f = open("demofile.txt" , "r")
+print(f.read())
+
+#QWhat happens to the original file content if you open a file like this:
+f = open('demofile3.txt', 'w')
+#A . The original content will be overwritten 
+
+#how to create a file 
+f =open("demofile3.txt" , "w")
+f.write("Yay we have created a new file with content")
+
+
+# How to delete files 
+import os
+os.remove("demofile3.txt")
+
+#how to make sure the file exsists 
+import os
+if os.path.exists("demofile3.text") :
+    os.remove("demofile3.txt")
+else : 
+    print("The file does not exsist")
