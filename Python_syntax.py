49014dfa33abd6dba5fed57e19ba67e92e597656
Alicia Bonavita <aliciamariabonavita@gmail.com>
2024-09-27 15:32:07 -0400

    Create Python_syntax.py

---
+++ Python_syntax.py
@@ -0,0 +1,32 @@
+# python indentation - identation referes to the space at the begginging of a code line 
+#exampele
+If 5 > 2: 
+  Print("Five is greater than two!")
+# Python variables are created to assign something a value 
+#ex 
+x = 5
+y = "Hello,World!" 
+#upper and lower case does matter as it differentiates between different variables 
+
+# Casting allows you to specify data types of variables 
+#ex
+x = str('hello')
+y = int(7)
+
+# Geting type by using the following syntax 
+x = 5
+print(type(x))
+
+#variable naming 
+# - must start with a letter or a underscore character 
+# a variable name cannot start with a number 
+# a variable name can only contain alpha-numeric characters and underscores 
+# variable names are case sensitive 
+# variable name cannot be any of the python keywords 
+#ex
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
